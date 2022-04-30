110b0c85c40b5d3d9eece8b58b30784e9b2078db
SaketThota <saketthota98@gmail.com>
2022-04-30 23:29:12 +0530

    Assigned content.js work

--- content.js
+++ content.js
@@ -1,1 +1,8 @@
 document.body.style.backgroundColor = 'orange';
+
+
+/*
+1. Amazon, flipkart button after product title.
+2. Button UI
+3. Get the price of the product name, product link, product price.
+*/
